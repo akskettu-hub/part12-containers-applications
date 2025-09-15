39919c04520dffc131d1add87c337077b3cfe550
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-09-15 15:51:14 +0300

    Implemented getting single to do.

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,19 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  //res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  //res.sendStatus(405); // Implement this
+  const savedTodo = await Todo.save({
+    text: req.body.text,
+    done: false
+  })
+
+  res.send(savedTodo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
