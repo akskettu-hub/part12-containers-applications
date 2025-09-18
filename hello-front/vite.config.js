afecbc8aea1789e98817b182e3fd109454341ed6
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-09-18 11:31:32 +0300

    Got busybox mostly working. Had to modify vite config to get it to work

--- hello-front/vite.config.js
+++ hello-front/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+
+  server: {
+    host: true,
+    strictPort: true,
+    cors: true,
+    origin: 'http://app:5173',
+  },
 })
